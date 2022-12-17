6b26c43d41bdbe7e4858fff26e5b3af35816b129
biancapower <bianca@biancapower.com>
2022-12-17 17:28:02 +1100

    Two examples of custom queries

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -7,4 +7,10 @@ class Types::AuthorType < Types::BaseObject
   field :is_alive, Boolean, null: false
   field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  field :full_name, String, null: false
+
+  field :backwards_name, String, null: false, description: "The author's name, written backwards"
+  def backwards_name
+    "#{object.last_name}, #{object.first_name}"
+  end
 end
